b2c2d5545f542a610af865cf455744f0be567aa1
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2022-12-07 01:38:46 -0300

    criado filtro

--- psicologia/src/main/java/com/example/psicologia/repository/paciente/PacienteRepositoryImpl.java
+++ psicologia/src/main/java/com/example/psicologia/repository/paciente/PacienteRepositoryImpl.java
@@ -1,12 +1,88 @@
 package com.example.psicologia.repository.paciente;
 
+import com.example.psicologia.entity.Paciente;
+import com.example.psicologia.repository.filter.PacienteFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PacienteRepositoryImpl {
+public class PacienteRepositoryImpl implements PacienteRepositoryQuery {
 
 
     @PersistenceContext
     private EntityManager manager;
+
+
+    @Override
+    public Page<Paciente> filtar(PacienteFilter pacienteFilter , Pageable pageable) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Paciente> criteria = builder.createQuery(Paciente.class);
+        Root<Paciente> root = criteria.from(Paciente.class);
+
+
+        //criar restricoes
+        Predicate[] predicates = criarRestricoes(pacienteFilter,builder,root);
+        criteria.where(predicates);
+
+        TypedQuery<Paciente> query = manager.createQuery(criteria);
+
+        adicionarRestricoeaDePaginacao(query,pageable);
+
+        return new PageImpl<>(query.getResultList(),pageable,total(pacienteFilter));
+    }
+
+    private Predicate[] criarRestricoes(
+            PacienteFilter pacienteFilter
+            ,CriteriaBuilder  builder ,
+            Root<Paciente> root){
+
+        List<Predicate> predicates = new ArrayList<>();
+
+        if(pacienteFilter.getNome()!=null){
+            predicates.add(builder.like(
+                    builder.lower(root.get("nome")),
+                    "%" +
+                            pacienteFilter.
+                                    getNome().
+                                    toLowerCase() + "%"
+            ));
+        }
+
+        return  predicates.toArray(new Predicate[predicates.size()]);
+    }
+
+    private void adicionarRestricoeaDePaginacao(
+            TypedQuery<Paciente> query,
+            Pageable pageable){
+        int paginalAtual = pageable.getPageNumber();
+        int totalRegistrosPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginalAtual * totalRegistrosPorPagina;
+        query.setFirstResult(primeiroRegistroDaPagina);
+        query.setMaxResults(totalRegistrosPorPagina);
+    }
+
+    private Long total(PacienteFilter pacienteFilter){
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Paciente> root = criteria.from(Paciente.class);
+
+        //criar restricoes
+        Predicate[] predicates = criarRestricoes(pacienteFilter,builder,root);
+        criteria.where(predicates);
+
+        criteria.select(builder.count(root));
+        return manager.createQuery(criteria).getSingleResult();
+    }
+
 
 }
