b64ff52eff91beef0eac537bdf95e29985aa48a2
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2022-12-13 23:34:42 -0300

    comecando pegar pacientes

--- frontend/js/pacientes.js
+++ frontend/js/pacientes.js
@@ -1,1 +1,12 @@
-alert("ola")
+const tabelaInicial = () =>{
+    const url = `http://localhost:8080/paciente`
+    fetch(url)
+    .then(response =>response.json())
+    .then((teste) =>  
+
+        teste.content.map((test,indice)=> console.log(indice))
+    
+    )
+    
+}
+tabelaInicial()
