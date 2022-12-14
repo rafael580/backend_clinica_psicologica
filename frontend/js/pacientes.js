638b462851524a5df516cd262d61811c61698cf4
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2022-12-14 14:45:51 -0300

    get, delete, paciente ok

--- frontend/js/pacientes.js
+++ frontend/js/pacientes.js
@@ -1,5 +1,4 @@
-const corpoTabela = document.querySelector(".corpoTabela")
-const bode = document.createElement('tbody')
+const corpoTabela = document.querySelector(".tabelinha")
 const tabelaInicial = () =>{
     const url = `http://localhost:8080/paciente`
     fetch(url)
@@ -13,18 +12,63 @@ const tabelaInicial = () =>{
             const endereco = document.createElement('td')
             const email  = document.createElement('td')
             const genero = document.createElement('td')
+            const estado = document.createElement('td')
             const cidade = document.createElement('td')
-            const municipio = document.createElement('td')
+            const botoes = document.createElement('td')
+            const editarA  = document.createElement("a")
+            const excluirE = document.createElement("a")
+            const botaoEditar = document.createElement('button')
+            const botaoExcluir  = document.createElement('button')
 
             nome.innerHTML = test.nome
             endereco.innerHTML = test.endereco
-            console.log(test.endereco)
+            email.innerHTML = test.email
+            genero.innerHTML = test.genero.nome
+            estado.innerHTML = test.estado
+            cidade.innerHTML = test.cidade
+
+            botaoEditar.innerHTML = "editar"
+            botaoExcluir.innerHTML = "excluir"
+
+            botaoExcluir.setAttribute('onclick', "deletar(" + test.id + ")")
+            botaoEditar.setAttribute('onclick','editarPaciente('+test.id+')')
+
+            editarA.appendChild(botaoEditar)
+            excluirE.appendChild(botaoExcluir)
+
+            botoes.appendChild(editarA)
+            botoes.appendChild(excluirE)
+
             linha.appendChild(nome)
             linha.appendChild(endereco)
-            bode.appendChild(linha)
-            console.log(corpoTabela)
+            linha.appendChild(email)
+            linha.appendChild(genero)
+            linha.appendChild(estado)
+            linha.appendChild(cidade)
+            linha.appendChild(botoes)
+
+
             corpoTabela.appendChild(linha)
 
         }))   
 }
+
+function deletar(id){
+    const url = ` http://localhost:8080/paciente/${id}`;
+
+    fetch(url,
+        {
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            method: "DELETE"
+            
+        }) 
+
+}
+function editarPaciente(id){
+    console.log(id)
+}
+
 tabelaInicial()
